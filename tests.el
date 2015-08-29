48d0649af9c75019bfcfaef1ad42a6b4293377e0
Andrea Turso <trashofmasters@gmail.com>
2015-08-29 19:47:23 +0100

    Basic resolution in place, but plenty of polishing required.

--- tests.el
+++ tests.el
@@ -34,7 +34,7 @@
 (ert-deftest semantic-php-name-resolution-rule-2 ()
   (let ((semantic-php-aliased-names (list (cons "C" "A\\B\\C"))))
     ;; (should (equal (list "A\\B\\C\\D\\e")
-                   (semantic-php-resolve-name "C\\D\\e" nil)))))
+    (semantic-php-resolve-name "C\\D\\e" nil)))
 
 ;;
 ;; 3. Inside a namespace, all qualified names not translated according
@@ -133,20 +133,30 @@ qualified or unqualified symbols."
   "Resolves a possibly aliased symbol SYMBOL-NAME info a FQN.
 
 If the symbol is an alias in KNOWN-ALIASES the corresponding
-fully qualified name is returned."
-  (let ((aliased-name (assoc-default symbol-name semantic-php-aliased-names)))
+fully qualified name is returned.
+
+If the first component of SYMBOL-NAME is a known alias, then the
+this part is replaced with the full namespace name."
+  (let* ((name-parts (split-string symbol-name "\\\\"))
+         (aliased-ns (assoc-default (car name-parts) semantic-php-aliased-names))
+         (aliased-name (assoc-default symbol-name semantic-php-aliased-names)))
     (if aliased-name
         aliased-name
-      symbol-name)))
+      (if aliased-ns
+          (semantic-php--name-prepend-ns
+           (mapconcat 'identity (cdr name-parts) "\\")
+           (semantic-php--name-dereference (car name-parts)))
+        symbol-name))))
 
 (defun semantic-php--name-qualified-alternatives (symbol-name current-ns)
   "Given an unqualified SYMBOL-NAME it returns a list of is
 possible qualified names as available in CURRENT-NS."
-  (if current-ns
-      (list (semantic-php--name-prepend-ns symbol-name current-ns) symbol-name)
-    ;; We're in the global namespace, so we return the singleton
-    ;; list with symbol-name.
-    (list symbol-name)))
+    (if current-ns
+        (append (list (semantic-php--name-prepend-ns symbol-name current-ns))
+                (list (semantic-php--name-dereference symbol-name)))
+      (delete-dups (list
+       symbol-name
+       (semantic-php--name-dereference symbol-name)))))
 
 (ert-deftest semantic-php-name-resolution-unqualified-in-global ()
   "Tests the resolution of an unqualified name in a the global namespace."
@@ -214,22 +224,22 @@ possible qualified names as available in CURRENT-NS."
          (list (cons "TestDate" "Carbon\\Carbon")
                (cons "TestCase" "PHPUnit_Framework_TestCase"))))
     (should (equal
-             (list "Carbon\\Carbon")
+             (list "TestDate" "Carbon\\Carbon")
              (semantic-php-resolve-name "TestDate" nil)))
     (should (equal
              (list "Version")
              (semantic-php-resolve-name "Version" nil)))))
 
-(ert-deftest semantic-php-name-resolution-unqualified-with-alias-in-global ()
-  (let ((semantic-php-aliased-names
-         (list (cons "TestDate" "Carbon\\Carbon")
-               (cons "TestCase" "PHPUnit_Framework_TestCase"))))
-    ;; (should (equal
-    ;;          (list "MainNs\\TestCase" "PHPUnit_Framework_TestCase")
-    ;;          (semantic-php-resolve-name "TestCase" "MainNs")))
-    ;; (should (equal
-             (list "MainNs\\TestDate" "TestDate" "Carbon\\Carbon")
-             (semantic-php-resolve-name "TestDate" "MainNs")))))
+;; (ert-deftest semantic-php-name-resolution-unqualified-with-alias-in-global ()
+;;   (let ((semantic-php-aliased-names
+;;          (list (cons "TestDate" "Carbon\\Carbon")
+;;                (cons "TestCase" "PHPUnit_Framework_TestCase"))))
+;;     ;; (should (equal
+;;     ;;          (list "MainNs\\TestCase" "PHPUnit_Framework_TestCase")
+;;     ;;          (semantic-php-resolve-name "TestCase" "MainNs")))
+;;     ;; (should (equal
+;;              ;; (list "MainNs\\TestDate" "TestDate" "Carbon\\Carbon")
+;;              (semantic-php-resolve-name "TestDate" "MainNs")))))
 
 (ert-deftest semantic-php-name-resolution-with-alias-and-no-known-aliases ()
     "Tests resolving a unqualified name without any known aliases."
@@ -244,6 +254,13 @@ possible qualified names as available in CURRENT-NS."
     (should (equal
              "Carbon\\Carbon"
              (semantic-php--name-dereference "TestDate")))))
+
+(ert-deftest semantic-php-name-resolution-with-aliased-ns ()
+  "Tests the resolution of a symbol that has been aliased."
+  (let ((semantic-php-aliased-names (list (cons "C" "A\\B\\C"))))
+    (should (equal
+             "A\\B\\C\\D\\E"
+             (semantic-php--name-dereference "C\\D\\E")))))
 
 (provide 'tests)
 
